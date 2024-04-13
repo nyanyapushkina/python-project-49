327573833b4f77a4b81a0ff6deadce3786a54bc8
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-04-13 12:10:11 +0300

    Add a separate function to check if the number is even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,6 +2,8 @@ from random import randint
 
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
+def is_even(number):
+    return number % 2 == 0
 
 def get_data():
     # Generate a random number and create a question
@@ -9,5 +11,5 @@ def get_data():
     game_question = f'Question: {number_to_check}'
 
     # If the number is exactly divisible by 2, it is even
-    correct_answer = 'yes' if number_to_check % 2 == 0 else 'no'
+    correct_answer = 'yes' if is_even(number_to_check) else 'no'
     return game_question, correct_answer
