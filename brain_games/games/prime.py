94063f758771eb19a4937b9518973ddec29431e0
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-04-13 13:08:47 +0300

    Improve readability of prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,6 +2,7 @@ from random import randint
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+
 def is_prime(number):
     d = 2
     while d * d <= number and number % d != 0:
@@ -12,7 +13,7 @@ def is_prime(number):
 def get_data():
     number_to_check = randint(0, 1000)
     game_question = f'Question: {number_to_check}'
-    
+
     correct_answer = 'yes' if is_prime(number_to_check) else 'no'
 
     return game_question, correct_answer
