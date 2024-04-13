ca56b2c895ce2773cf18a97fceed1066e7a28660
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-04-13 12:49:30 +0300

    Add a separate function to check if the number is prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,17 +2,17 @@ from random import randint
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+def is_prime(number):
+    d = 2
+    while d * d <= number and number % d != 0:
+        d += 1
+    return d * d > number
+
 
 def get_data():
     number_to_check = randint(0, 1000)
     game_question = f'Question: {number_to_check}'
-
-    d = 2
-    while d * d <= number_to_check and number_to_check % d != 0:
-        d += 1
-    if d * d > number_to_check:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    
+    correct_answer = 'yes' if is_prime(number_to_check) else 'no'
 
     return game_question, correct_answer
