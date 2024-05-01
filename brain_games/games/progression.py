d8247435a4a2db479259b15b16dc98951098ce91
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-05-01 16:36:45 +0300

    Change varaibles's names in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,15 +2,24 @@ from random import randint
 
 RULES = 'What number is missing in the progression?'
 
+FIRST_TERM_MIN = 0
+FIRST_TERM_MAX = 100
 
-def generate_progression(start_number, progression_step, progression_length):
+COMMON_DIFFERENCE_MIN = 1
+COMMON_DIFFERENCE_MAX = 10
+
+NUMBER_OF_TERMS_MIN = 5
+NUMBER_OF_TERMS_MAX = 10
+
+
+def generate_progression(first_term, common_difference, number_of_terms):
 
     progression = []
 
     i = 0
     # Fill the progression with all the numebers
-    while i < progression_length:
-        progression.append(str(start_number + progression_step * i))
+    while i < number_of_terms:
+        progression.append(str(first_term + common_difference * i))
         i += 1
 
     return progression
@@ -26,12 +35,13 @@ def hide_element(progression, hidden_number_index):
 
 def get_data():
     # Generate the number to start a progression with
-    start = randint(0, 100)
+    first_term = randint(FIRST_TERM_MIN, FIRST_TERM_MAX)
     # Generate a step for an arithmetic progression
-    step = randint(1, 10)
+    common_difference = randint(COMMON_DIFFERENCE_MIN, COMMON_DIFFERENCE_MAX)
     # Define the length of the progression, i.e the number of elements
-    length = randint(5, 10)
-    progression = generate_progression(start, step, length)
+    number_of_terms = randint(NUMBER_OF_TERMS_MIN, NUMBER_OF_TERMS_MAX)
+    progression = generate_progression(first_term, common_difference,
+                                       number_of_terms)
     hidden_number_index = randint(0, len(progression) - 1)
 
     correct_answer = progression[hidden_number_index]
